51d7dc8bb77286e0c7b7535520eb5037c93b8f61
waretada <maks_koneko@mail.ru>
2020-07-15 15:16:35 +0300

    Comments' text editing

--- Algorithm.py
+++ Algorithm.py
@@ -28,29 +28,29 @@ def game_core_v3a(number):
     """
     
     count = 1                          # The attempt's counter.The 1-st attempt.
-    predict = 100                      # Setting value for the 1-st attempt.
+    predict = 100                      # Setting predictable value for the 1-st attempt.
     if number == predict:              # Is the right end of interval (1;100) the desired number? 
         pass
     
-    else:                               # if the 1-st attempt is not successful
+    else:                               # If the 1-st attempt is not successful...
         # At every next attempt: count it, take the next search interval 
         # and check whether its 'middle' what is needed or not.
-        left_p = 1        # The left point of the previous,the 1-st, search interval.
+        left_p = 1        # The left point of the previous,i.e.the 1-st, search interval.
         right_p = 100     # The right point of the search interval mentioned above.    
     
         while number != predict:    # Repeat procedure below until find the desired number 
             count+=1 
             if number < predict:  
-                # If the desired number is less than the 'middle' of the previous interval, 
+                # If the desired number is less than the 'middle' of the previous search interval, 
                 # take the left half of the previous one as the next interval.
                 right_p = predict
                 predict = get_mid(left_p,right_p)  
                 
             elif number > predict:
-                # If the desired number is more than the 'middle' of the previous interval,
+                # If the desired number is more than the 'middle' of the previous search interval,
                 # take the right half of the previous one as the next interval.
                 left_p = predict
                 predict = get_mid(left_p,right_p)       
                        
-    return(count)                        # return how many attempt did it take
+    return(count)                        # Return how many attempts did it take
 
